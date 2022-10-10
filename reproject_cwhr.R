62fb7a583296f1a703149c61ac20d6e52c964b4e
Sam Flake <33131088+flakesw@users.noreply.github.com>
2022-10-10 12:13:45 -0400

    Update projection script

--- reproject_cwhr.R
+++ reproject_cwhr.R
@@ -8,6 +8,8 @@ setGDALconfig("OSR_USE_NON_DEPRECATED", value="NO") #so we can still use EPSG:21
 tcsi_mask_9311 <- rast("./Models/Inputs/masks_boundaries/mask_9311_new.tif")
 crs(tcsi_mask_9311)
 
+tcsi_mask_nad83 <- rast("./Models/Inputs/masks_boundaries/mask_nad83.tif")
+
 
 tcsi_shape <- sf::st_read("./Models/Inputs/masks_boundaries/tcsi_area_shapefile/TCSI_v2.shp") %>% #is wgs84
   st_transform(crs = "EPSG:9311")
@@ -27,44 +29,25 @@ raster_list_fixed <- gsub(" ", "", raster_list) #1600 vegetation layers, 405 yea
 raster_list_fixed <- gsub("-", "_", raster_list_fixed)
 raster_list_fixed <- gsub("run", "Run", raster_list_fixed)
 raster_list_fixed <- gsub("all_species", "allspecies", raster_list_fixed)
-
-test <- map(raster_list_fixed, .f = ~str_split_fixed(., pattern = "_", 9)) %>%
-  do.call(rbind, .) 
-
-table(test[, 7])
-table(test[, 9])
-
-test2 <- test[test[, 7] == "predators", ]
-
-table(test2[, 1], test2[, 6]) 
-
-#missing 6 from scenario 6, 3 from scenario 3
-#equal beta, nestedness, and turnover missing
-
-#scenario3 year 20
-#scenario6 year 0 and 10
-
-#predators are missing 9 steps
-#herbivores has 1 extra step
+raster_list_fixed <- gsub("meanbeta", "beta", raster_list_fixed)
 
 
-
-out_folder <- "E:/TCSI LANDIS/Outputs_to_DHSVM_reprojected/"
+out_folder <- "E:/TCSI LANDIS/CWHR/CWHR_diversity_reprojected/"
 
 for(i in 1:length(raster_list)){
   
-  in_rast <- rast(raster_list[i])
-  # plot(in_rast)
-  # in_rast
+  in_rast <- rast(paste0(in_folder, "/", raster_list[i]))
+  #plot(in_rast)
+  #in_rast
   
   in_rast_reprojected <- tcsi_mask_9311
   values(in_rast_reprojected) <- values(in_rast)
-  # plot(in_rast_reprojected)
+  plot(in_rast_reprojected)
   
-  in_rast_dhsvm <- project(in_rast_reprojected, dhsvm_grid, method = "near")
-  # plot(in_rast_dhsvm)
-  # in_rast_dhsvm
+  in_rast_cwhr <- project(in_rast_reprojected, tcsi_mask_nad83, method = "near")
+ #plot(in_rast_cwhr)
+ #in_rast_cwhr
   
-  raster_name <- gsub(" ", "", basename(raster_list[i]))
-  writeRaster(in_rast_dhsvm, paste0(out_folder, raster_name), overwrite = TRUE)
+  raster_name <- gsub(" ", "", basename(raster_list_fixed[i]))
+  writeRaster(in_rast_cwhr, paste0(out_folder, raster_name), overwrite = TRUE)
 }
